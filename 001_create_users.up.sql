44beda805bb02fa0fa819e86aa5329d02c3014de
aktivgo <aktivgo01062001@gmail.com>
2022-02-26 22:56:45 +0300

    Add activation link and refresh token

--- 001_create_users.up.sql
+++ 001_create_users.up.sql
@@ -4,8 +4,10 @@ CREATE TABLE users (
     email           varchar(255)            NOT NULL,
     hashPassword    varchar(255)            NOT NULL,
     isActivated     tinyint(1)              NOT NULL DEFAULT 0,
-    role            enum('user', 'admin')   NOT NULL DEFAULT 'user'
+    activationLink  varchar(255)            NULL,
+    role            enum('user', 'admin')   NOT NULL DEFAULT 'user',
+    refreshToken    varchar(255)            NOT NULL
 );
 
-INSERT INTO users (fullName, email, hashPassword, isActivated, role) VALUE
-    ('Администратор', 'admin@gmail.dev', '$2a$04$oz.EwJV/72QMqova.sBna.94Xt3B8PCFbb878Q9DJquKZ7Rdjezlm', 1, 'admin');
+INSERT INTO users (fullName, email, hashPassword, isActivated, role, refreshToken) VALUE
+    ('Администратор', 'admin@gmail.dev', '$2a$04$oz.EwJV/72QMqova.sBna.94Xt3B8PCFbb878Q9DJquKZ7Rdjezlm', 1, 'admin', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJmdWxsTmFtZSI6ItCQ0LTQvNC40L3QuNGB0YLRgNCw0YLQvtGAIiwicm9sZSI6ImFkbWluIn0.1CkY6SiX6cIaEeAlCwH4CLa6wrgr4E87tkv4xrW0kyc');
